6e2c5cce20542add81f975aba8caf16af8755e4b
Raafi Rahman <rafhay101@gmail.com>
2021-10-18 14:19:40 -0400

    Update HW2.py

--- HW2.py
+++ HW2.py
@@ -7,14 +7,12 @@ from numpy.random.mtrand import logistic
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import axes3d
 import seaborn as sns
-from seaborn import categorical
 
 import sklearn.linear_model as linMod
 from sklearn.metrics import mean_squared_error, r2_score, confusion_matrix
-from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
-from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis, QuadraticDiscriminantAnalysis
+from sklearn.neighbors import KNeighborsClassifier
 
 import statsmodels.api as sm
 import statsmodels.formula.api as smf
@@ -24,10 +22,10 @@ import os
 
 # Question 10 ==============================================
 
-# Laptop Path
+# Laptop Path (For my personal machine)
 # UP_DIR = "C:\\Users\\Rafha\\OneDrive\\Documents\\GitHub\\STAT72401\\Datasets"
 
-# Desktop Path
+# Desktop Path (For my personal machine)
 UP_DIR = "C:\\Users\\Rafha\\Desktop\\Code\\STAT72401\\Datasets"
 
 csvPath = os.path.join(UP_DIR, 'Weekly.csv')
@@ -42,20 +40,19 @@ print(weeklyData.head())
 
 
 # Part A ---------------------------------------------------
+print('\nPart A ---------------------------------------------------\n')
 
-print("""\n(A) Produce some numerical and graphical summaries of the Weekly data.
-    Do there appear to be any patterns?\n""")
-
-# Statistical facts
+# Statistical summary
 print(weeklyData.describe(), "\n")
 
 # Pairwise correlation
 print(weeklyData.corr())
 
-# Pairplot
-# Un-quote to see graph
-# sns.pairplot(weeklyData)
-# plt.show()
+# Pairplot (Un-quote to see)
+"""
+sns.pairplot(weeklyData)
+plt.show()
+"""
 
 print("""As the year increases, so does volume.
 If today is positive then direction is 'up' and vice-versa.
@@ -63,6 +60,7 @@ Everything else seems uniform.""")
 
 
 # Part B ---------------------------------------------------
+print('\nPart B ---------------------------------------------------\n')
 
 # Scikit Learn
 X = weeklyData[['Lag1', 'Lag2', 'Lag3', 'Lag4', 'Lag5', 'Volume']]
@@ -83,7 +81,9 @@ print(ModelStats.summary())
 
 print('Lag2 has a p-value of 0.03, implying statistical significance')
 
+
 # Part C ---------------------------------------------------
+print('\nPart C ---------------------------------------------------\n')
 
 ConfuseMatrix = confusion_matrix(y, LogRegress.predict(X))
 print(ConfuseMatrix)
@@ -95,7 +95,14 @@ print(ConfuseMatrix)
 print('''54 "down" days and 557 "up" days were correctly classified.
 The model resulted in 430 false ups and 48 false downs.''')
 
+
 # Part D ---------------------------------------------------
+print('\nPart D ---------------------------------------------------\n')
+
+# The model would be better if training data and testing data was
+# assigned randomly. We also know market conditions change over time
+# and are based on the time they are from. This way of splitting
+# data by years would be very bad in actual practice.
 
 trainData = weeklyData[(weeklyData['Year'] >= 1990) &
                        (weeklyData['Year'] <= 2008)]
@@ -118,7 +125,9 @@ print(confusion_matrix(yTest, LogRegress.predict(XTest)))
 
 print('Accuracy of Logistic Regression: ', LogRegress.score(XTest, yTest))
 
+
 # Part E ---------------------------------------------------
+print('\nPart E ---------------------------------------------------\n')
 
 LDA = LinearDiscriminantAnalysis()
 LDA.fit(XTrain, yTrain)
@@ -127,7 +136,9 @@ print(confusion_matrix(yTest, LDA.predict(XTest)))
 
 print('Accuracy of Linear Discriminant Analysis: ', LDA.score(XTest, yTest))
 
+
 # Part F ---------------------------------------------------
+print('\nPart F ---------------------------------------------------\n')
 
 QDA = QuadraticDiscriminantAnalysis()
 QDA.fit(XTrain, yTrain)
@@ -136,18 +147,53 @@ print(confusion_matrix(yTest, QDA.predict(XTest)))
 
 print('Accuracy of Quadratric Discriminant Analysis: ', QDA.score(XTest, yTest))
 
+
 # Part G ---------------------------------------------------
+print('\nPart G ---------------------------------------------------\n')
 
-# K NEAREST NEIGHBORS
+KNN = KNeighborsClassifier(n_neighbors=1)
+KNN.fit(XTrain, yTrain)
 
-LDA = LinearDiscriminantAnalysis()
-LDA.fit(XTrain, yTrain)
+print(confusion_matrix(yTest, KNN.predict(XTest)))
 
-print(confusion_matrix(yTest, LDA.predict(XTest)))
-
-print('Accuracy of Linear Discriminant Analysis: ', LDA.score(XTest, yTest))
+print('Accuracy of K-Nearest Neighbors: ', KNN.score(XTest, yTest))
 
 # Part H ---------------------------------------------------
+print('\nPart H ---------------------------------------------------\n')
+
+print('Accuracy of Logistic Regression: ', LogRegress.score(XTest, yTest))
+print('Accuracy of Linear Discriminant Analysis: ', LDA.score(XTest, yTest))
+print('Accuracy of Quadratric Discriminant Analysis: ', QDA.score(XTest, yTest))
+print('Accuracy of K-Nearest Neighbors: ', KNN.score(XTest, yTest))
+
+print('''Logistic Regression and LDA provide the best results, followed by
+QDA and then K-Nearest Neighbors.''')
 
 
 # Part I ---------------------------------------------------
+print('\nPart I ---------------------------------------------------\n')
+
+# Test different K values in K Nearest Neighbors
+BestK = 0
+BestKScore = 0
+
+for K in range(1, 984):  # Test all possible K values
+    KNN = KNeighborsClassifier(n_neighbors=K)
+    KNN.fit(XTrain, yTrain)
+
+    if KNN.score(XTest, yTest) > BestKScore:  # Update best K value so far
+        BestKScore = KNN.score(XTest, yTest)
+        BestK = K
+
+print('Best K value is', BestK)
+
+KNN = KNeighborsClassifier(n_neighbors=BestK)
+KNN.fit(XTrain, yTrain)
+
+print(confusion_matrix(yTest, KNN.predict(XTest)))
+
+print('Accuracy of K-Nearest Neighbors with K =',
+      BestK, ':', KNN.score(XTest, yTest))
+
+print('''We see that with a K = 153, we obtain better results than
+Logistic Regression, LDA, or QDA.''')
